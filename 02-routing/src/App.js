2a948655acebb1fb7498f564911a1fb7bd24c934
hjzjz7020 <a18042681258@gmail.com>
2021-09-05 18:49:39 +1200

    add header and corresponding .css file

--- 02-routing/src/App.js
+++ 02-routing/src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupPage />
