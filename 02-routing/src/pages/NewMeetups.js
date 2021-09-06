513dc8a45a8cb8221e703827c2db88ae90fc9535
hjzjz7020 <a18042681258@gmail.com>
2021-09-06 18:18:23 +1200

    configure a Firebase server to host dummy meetup data

--- 02-routing/src/pages/NewMeetups.js
+++ 02-routing/src/pages/NewMeetups.js
@@ -1,5 +1,23 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New meetup page.</div>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://learn-react-38d27-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
