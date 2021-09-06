4806cdaea3b4a2fa4a922aa8ce874f9d8fd496a8
hjzjz7020 <a18042681258@gmail.com>
2021-09-07 00:49:28 +1200

    fetch data from Firebase and load it on index using useEffect

--- 02-routing/src/pages/NewMeetups.js
+++ 02-routing/src/pages/NewMeetups.js
@@ -1,6 +1,8 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
   function addMeetupHandler(meetupData) {
     fetch(
       "https://learn-react-38d27-default-rtdb.firebaseio.com/meetups.json",
@@ -9,7 +11,9 @@ function NewMeetupPage() {
         body: JSON.stringify(meetupData),
         headers: { "Content-Type": "application/json" },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
