48b567a258444d3510d759fc1b70db1637200e07
hjzjz7020 <a18042681258@gmail.com>
2021-09-06 15:26:50 +1200

    add card layout in the props.children way.

--- 02-routing/src/App.js
+++ 02-routing/src/App.js
@@ -2,12 +2,11 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMeetupPage />
@@ -19,7 +18,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
